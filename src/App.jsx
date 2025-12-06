e2b883e9c8758926b4323390b4337d59602e1429
Dugiii <dugidemeg@gmail.com>
2025-12-06 00:42:58 -0600

    implementing calculator logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import { useReducer } from 'react'
+import { ACTIONS } from './Constants'
+import { DigitButton } from './DigitButton'
+import { OperationButton } from './OperationButton'
 import './App.css'
 
-const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate',
-}
 
 function reducer(state, { type, payload }) {
-
   switch (type) {
-    case ACTIONS.ADD_DIGIT:
+    case ACTIONS.CLICK_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") { // curly brackets are optional since it could be on a single line
+        return state; // do nothing if the digit is 0 and the user tries adding multiple 0s
+      }
+
+      if (payload.digit === "." && state.currentOperand.includes(".")) { 
+        return state; // do nothing if the digit is . and the user tries adding multiple decimal points
+      }
+
       return {
         ...state,
         currentOperand: `${state.currentOperand || ''}${payload.digit}`,
       }
-    // default:
-    //   return state
+    
+    case ACTIONS.CLEAR:
+      return{}
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state;
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          previousOperand: state.currentOperand, // store the currentOperand as the previousOperand
+          operation: payload.operation,
+          currentOperand: null, // reset the currentOperand so that it can take new values
+        }
+      }
+
+
   }
 }
 
 function App() {
-  const [{ currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
-
-  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 } })
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
 
   return (
     <div className="calculator-grid">
@@ -34,23 +52,37 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
 
-      <button className="span-two">AC</button>
+      <button 
+        className="span-two" 
+        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+      >
+        AC
+      </button>
+
       <button>DEL</button>
-      <button>÷</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+
+      <OperationButton operation="÷" dispatch={dispatch}/>
+
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+
+      <OperationButton operation="*" dispatch={dispatch}/>
+
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+
+      <OperationButton operation="+" dispatch={dispatch}/>
+
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+
+      <OperationButton operation="—" dispatch={dispatch}/>
+
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
       <button className="span-two">=</button>
     </div>
   )
